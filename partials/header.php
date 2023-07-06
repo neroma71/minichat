420b7263485bd50d1053d57c626d2291fb708c84
neroma71 <neroma71@gmail.com>
2023-07-06 14:13:02 +0200

    start minichat

---
+++ partials/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<header>
+    <h1>welcome on my site</h1>
+    <nav>
+        <ul>
+            <li><a href="index.php">home</a></li>
+            <li><a href="tchat.php">tchat</a></li>
+        </ul>
+    </nav>
+</header>
